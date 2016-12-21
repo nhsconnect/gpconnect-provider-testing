37d53c66c9d96386ebd2d9af26ff54a491484b17
michaelm <michael.measures@controltrack.com.au>
2016-12-22 07:09:24 +1030

    Added support for using the @manual tag to skip scenarios.

---
+++ GPConnect.Provider.AcceptanceTests/Steps/ManualSteps.cs
@@ -0,0 +1,24 @@
+﻿using TechTalk.SpecFlow;
+
+// https://decoupledlogic.wordpress.com/2013/12/10/specflow-manual-testing/
+namespace GPConnect.Provider.AcceptanceTests.Steps
+{
+    [Binding, Scope(Tag = "Manual")]
+    public class ManualSteps
+    {
+        [Given(".*"), When(".*"), Then(".*")]
+        public void EmptyStep()
+        {
+        }
+
+        [Given(".*"), When(".*"), Then(".*")]
+        public void EmptyStep(string multiLineStringParam)
+        {
+        }
+
+        [Given(".*"), When(".*"), Then(".*")]
+        public void EmptyStep(Table tableParam)
+        {
+        }
+    }
+}
