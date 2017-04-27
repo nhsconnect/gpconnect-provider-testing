99efa1082357df6251ddc71aca5e912732286abd
Chris Jobling <christopher.jobling@answerdigital.com>
2017-04-27 11:12:13 +0100

    Added properties to get the file ready for development

--- GPConnect.Provider.AcceptanceTests/Steps/BookAppointmentSteps.cs
+++ GPConnect.Provider.AcceptanceTests/Steps/BookAppointmentSteps.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using GPConnect.Provider.AcceptanceTests.Context;
+using GPConnect.Provider.AcceptanceTests.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TechTalk.SpecFlow;
 
 namespace GPConnect.Provider.AcceptanceTests.Steps
 {
-    class BookAppointmentSteps
+    [Binding]
+    class BookAppointmentSteps : TechTalk.SpecFlow.Steps
     {
+        private readonly FhirContext FhirContext;
+        private readonly HttpSteps HttpSteps;
+        private readonly HttpContext HttpContext;
+
+        // Headers Helper
+        public HttpHeaderHelper Headers { get; }
+
+        public BookAppointmentSteps(HttpHeaderHelper headerHelper, FhirContext fhirContext, HttpSteps httpSteps, HttpContext httpContext)
+        {
+            // Helpers
+            FhirContext = fhirContext;
+            Headers = headerHelper;
+            HttpSteps = httpSteps;
+            HttpContext = httpContext;
+        }
+
     }
 }
