e02bf98b033c77f949921bf61b597f9f8d116b92
James Cox <james.cox@answerdigital.com>
2017-04-27 15:00:41 +0100

    Added a step for searching and storing a patients current appointments

--- GPConnect.Provider.AcceptanceTests/Steps/AppointmentReadSteps.cs
+++ GPConnect.Provider.AcceptanceTests/Steps/AppointmentReadSteps.cs
@@ -28,12 +28,33 @@ namespace GPConnect.Provider.AcceptanceTests.Steps
         [Given(@"I find or create ""([^ ""] *)"" appointments for patient ""([^""]*)"" at organization ""([^""]*)"" and save a list of resources to ""([^""]*)""")]
         public void IFindOrCreateAAppointmentsForPatientAtOrganizationAndSaveAListOfResourceTo(string noApp, string patient, string organizaitonName, string appointmentListkey)
         {
-            //FhirContext.FhirOrganizations[organizaitonName]
-
-            Given($@"I perform a patient search for patient ""{patient}"" and store the first returned resources against key ""AppointmentReadPatientResource""");
-            // Search for above patient appointments
+            // Search For Patient appointments
+            Given($@"I search for patient ""{patient}"" appointments and save the returned bundle of appointment resources against key ""patientCurrentAppointments""");
 
         }
+
+
+        [Given(@"I search for patient ""([^""]*)"" appointments and save the returned bundle of appointment resources against key ""([^""]*)""")]
+        public void ISearchForPatientAppointmentsAndSaveTheReturnedBundleOfAppointmentResourcesAgainstKey(string patient, string patientAppointmentSearchBundleKey)
+        {
+            // Search For Patient
+            Given($@"I perform a patient search for patient ""{patient}"" and store the first returned resources against key ""AppointmentReadPatientResource""");
+
+            // Search For Patients Appointments
+            Patient patientResource = (Patient)HttpContext.StoredFhirResources["AppointmentReadPatientResource"];
+            Given($@"I am using the default server");
+            And($@"I am performing the ""urn:nhs:names:services:gpconnect:fhir:rest:search:patient_appointments"" interaction");
+            When($@"I make a GET request to ""/Patient/{patientResource.Id}/Appointment""");
+            Then($@"the response status code should indicate success");
+            And($@"the response body should be FHIR JSON");
+            And($@"the response should be a Bundle resource of type ""searchset""");
+
+            var returnedPatientAppointmentSearchBundle = (Bundle)FhirContext.FhirResponseResource;
+            if (HttpContext.StoredFhirResources.ContainsKey(patientAppointmentSearchBundleKey)) HttpContext.StoredFhirResources.Remove(patientAppointmentSearchBundleKey);
+            HttpContext.StoredFhirResources.Add(patientAppointmentSearchBundleKey, returnedPatientAppointmentSearchBundle);
+
+        }
+
 
         [Then(@"the response should be an Appointment resource")]
         public void theResponseShouldBeAnAppointmentResource()
@@ -41,6 +62,7 @@ namespace GPConnect.Provider.AcceptanceTests.Steps
             FhirContext.FhirResponseResource.ResourceType.ShouldBe(ResourceType.Appointment);
             Appointment appointment = (Appointment)FhirContext.FhirResponseResource;
         }
+
 
     }
 }
