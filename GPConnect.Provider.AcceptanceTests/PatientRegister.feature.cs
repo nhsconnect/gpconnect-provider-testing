c3b35ae580e8915f1714d3561f97c1c3cfca5077
James Cox <james.cox@answerdigital.com>
2017-07-17 08:48:24 +0100

    Changed feature name for register patient, removing character

--- GPConnect.Provider.AcceptanceTests/PatientRegister.feature.cs
+++ GPConnect.Provider.AcceptanceTests/PatientRegister.feature.cs
@@ -18,7 +18,7 @@ namespace GPConnect.Provider.AcceptanceTests
     [System.CodeDom.Compiler.GeneratedCodeAttribute("TechTalk.SpecFlow", "2.1.0.0")]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [NUnit.Framework.TestFixtureAttribute()]
-    [NUnit.Framework.DescriptionAttribute("PatientRegister`")]
+    [NUnit.Framework.DescriptionAttribute("PatientRegister")]
     [NUnit.Framework.CategoryAttribute("patient")]
     public partial class PatientRegisterFeature
     {
@@ -32,7 +32,7 @@ namespace GPConnect.Provider.AcceptanceTests
         public virtual void FeatureSetup()
         {
             testRunner = TechTalk.SpecFlow.TestRunnerManager.GetTestRunner();
-            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "PatientRegister`", null, ProgrammingLanguage.CSharp, new string[] {
+            TechTalk.SpecFlow.FeatureInfo featureInfo = new TechTalk.SpecFlow.FeatureInfo(new System.Globalization.CultureInfo("en-US"), "PatientRegister", null, ProgrammingLanguage.CSharp, new string[] {
                         "patient"});
             testRunner.OnFeatureStart(featureInfo);
         }
