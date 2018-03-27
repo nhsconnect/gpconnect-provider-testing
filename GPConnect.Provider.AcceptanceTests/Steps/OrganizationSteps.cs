8e4eba21de1aea0a6dbad8e1b8c4425d44a65b0b
James Cox <james.cox@answerdigital.com>
2018-03-27 15:16:25 +0100

    committed fix to missing method

--- GPConnect.Provider.AcceptanceTests/Steps/OrganizationSteps.cs
+++ GPConnect.Provider.AcceptanceTests/Steps/OrganizationSteps.cs
@@ -97,7 +97,6 @@
         {
             TheOrganizationMetadataShouldBeValid();
             TheOrganizationIdentifiersShouldBeValid();
-            TheOrganizationTypeShouldBeValid();
             TheLocationPartOfLocationShouldBeValid();
         }
 
