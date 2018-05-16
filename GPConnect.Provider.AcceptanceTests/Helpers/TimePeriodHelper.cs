f554e2f6e06cb895dcdba023a20dfba56671c24a
lucywilcock <lucy.wilcock@answerdigital.com>
2018-05-16 14:04:50 +0100

    Book appointments from tomorrow with appointment retrieve tests

--- GPConnect.Provider.AcceptanceTests/Helpers/TimePeriodHelper.cs
+++ GPConnect.Provider.AcceptanceTests/Helpers/TimePeriodHelper.cs
@@ -50,6 +50,12 @@
             return new Period(new FhirDateTime(date), new FhirDateTime(date.AddDays(days)));
         }
 
+        public static Period GetTimePeriodStartDateTomorrowEndDateDays(int days)
+        {
+            var date = DateTime.UtcNow.Date.AddDays(1);
+            return new Period(new FhirDateTime(date), new FhirDateTime(date.AddDays(days)));
+        }
+
         public static Period GetTimePeriodStartDateFormatEndDateFormat(string startDateFormat, string endDateFormat, int days = 3)
         {
             var date = DateTime.UtcNow;
