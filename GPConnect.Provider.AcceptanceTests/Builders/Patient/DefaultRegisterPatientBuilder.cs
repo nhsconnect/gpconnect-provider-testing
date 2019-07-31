0b3f886b43e2da5ad65b8276890d0463138ea58b
Sado1234 <sara.dominey@nhs.net>
2019-07-31 14:29:29 +0100

    Added meta.profile to request payload

--- GPConnect.Provider.AcceptanceTests/Builders/Patient/DefaultRegisterPatientBuilder.cs
+++ GPConnect.Provider.AcceptanceTests/Builders/Patient/DefaultRegisterPatientBuilder.cs
@@ -1,42 +1,42 @@
 ﻿namespace GPConnect.Provider.AcceptanceTests.Builders.Patient
 {
-    using System.Collections.Generic;
-    using Constants;
-    using Data;
-    using Helpers;
-    using Hl7.Fhir.Model;
+	using System.Collections.Generic;
+	using Constants;
+	using Data;
+	using Helpers;
+	using Hl7.Fhir.Model;
 
-    public class DefaultRegisterPatientBuilder
-    {
-        private readonly RegisterPatient _registerPatient;
+	public class DefaultRegisterPatientBuilder
+	{
+		private readonly RegisterPatient _registerPatient;
 
-        public DefaultRegisterPatientBuilder(RegisterPatient registerPatient)
-        {
-            _registerPatient = registerPatient;
-        }
+		public DefaultRegisterPatientBuilder(RegisterPatient registerPatient)
+		{
+			_registerPatient = registerPatient;
+		}
 
-        public Patient BuildPatient()
-        {
-            var patientIdentifier = new Identifier(FhirConst.IdentifierSystems.kNHSNumber, _registerPatient.SPINE_NHS_NUMBER);
+		public Patient BuildPatient()
+		{
+			var patientIdentifier = new Identifier(FhirConst.IdentifierSystems.kNHSNumber, _registerPatient.SPINE_NHS_NUMBER);
 
-            patientIdentifier.Extension.Add(new Extension
-            {
-                Url = FhirConst.StructureDefinitionSystems.kExtCcGpcNhsNumVerification,
-                Value = new CodeableConcept(FhirConst.CodeSystems.kCcNhsNumVerification, "01", "Number present and verified")
-            });
-			
+			patientIdentifier.Extension.Add(new Extension
+			{
+				Url = FhirConst.StructureDefinitionSystems.kExtCcGpcNhsNumVerification,
+				Value = new CodeableConcept(FhirConst.CodeSystems.kCcNhsNumVerification, "01", "Number present and verified")
+			});
+
 			var patientToRegister = new Patient
-            {
-                BirthDateElement = new Date(_registerPatient.DOB),
-                Name = new List<HumanName>
-                {
-                    NameHelper.CreateOfficialName(_registerPatient.NAME_GIVEN, _registerPatient.NAME_FAMILY)
-                },
-                Identifier = new List<Identifier>
-                {
-                    patientIdentifier
-                }
-            };
+			{
+				BirthDateElement = new Date(_registerPatient.DOB),
+				Name = new List<HumanName>
+				{
+					NameHelper.CreateOfficialName(_registerPatient.NAME_GIVEN, _registerPatient.NAME_FAMILY)
+				},
+				Identifier = new List<Identifier>
+				{
+					patientIdentifier
+				}
+			};
 
 			//SJD 31/07/2019 #269 meta.profile added to request payload
 			var patientMeta = new Meta();
@@ -48,22 +48,22 @@
 			patientToRegister.Meta = patientMeta;
 
 			switch (_registerPatient.GENDER)
-            {
-                case "MALE":
-                    patientToRegister.Gender = AdministrativeGender.Male;
-                    break;
-                case "FEMALE":
-                    patientToRegister.Gender = AdministrativeGender.Female;
-                    break;
-                case "OTHER":
-                    patientToRegister.Gender = AdministrativeGender.Other;
-                    break;
-                case "UNKNOWN":
-                    patientToRegister.Gender = AdministrativeGender.Unknown;
-                    break;
-            }
+			{
+				case "MALE":
+					patientToRegister.Gender = AdministrativeGender.Male;
+					break;
+				case "FEMALE":
+					patientToRegister.Gender = AdministrativeGender.Female;
+					break;
+				case "OTHER":
+					patientToRegister.Gender = AdministrativeGender.Other;
+					break;
+				case "UNKNOWN":
+					patientToRegister.Gender = AdministrativeGender.Unknown;
+					break;
+			}
 
-            return patientToRegister;
-        }
-    }
+			return patientToRegister;
+		}
+	}
 }
