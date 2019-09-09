ad4922806c8d1e59d8469c2fdc3458d1636d5855
Peter Gresty <peter.gresty@nhs.net>
2019-09-09 12:39:51 +0100

    Fixed URL for CapabilityStatement profile supports the GetStructuredRecord operation

--- GPConnect.Provider.AcceptanceTests/FHIR.feature.cs
+++ GPConnect.Provider.AcceptanceTests/FHIR.feature.cs
@@ -193,7 +193,7 @@ this.ScenarioInitialize(scenarioInfo);
 #line 44
  testRunner.And("the CapabilityStatement Operation \"gpc.getstructuredrecord\" has url \"https://fhir" +
                     ".nhs.uk/STU3/OperationDefinition/GPConnect-GetStructuredRecord-Operation-1/_hist" +
-                    "ory/1.8a\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+                    "ory/1.8\"", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line hidden
             this.ScenarioCleanup();
         }
