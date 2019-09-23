428fbab526b4701ee2a9f80a98d2a5207c5a3c2a
Peter Gresty <peter.gresty@nhs.net>
2019-09-23 12:06:10 +0100

    added in Consultations

---
+++ GPConnect.Provider.AcceptanceTests/Steps/StructuredConsultationsSteps.cs
@@ -0,0 +1,51 @@
+﻿namespace GPConnect.Provider.AcceptanceTests.Steps
+{
+    using Constants;
+    using Context;
+    using TechTalk.SpecFlow;
+    using Shouldly;
+    using Hl7.Fhir.Model;
+    using System.Collections.Generic;
+    using System;
+    using System.Linq;
+    using GPConnect.Provider.AcceptanceTests.Enum;
+    using static Hl7.Fhir.Model.Parameters;
+    using GPConnect.Provider.AcceptanceTests.Helpers;
+
+    [Binding]
+    public sealed class StructuredConsultationsSteps : BaseSteps
+    {
+        private readonly HttpContext _httpContext;
+        private List<Encounter> Encounters => _httpContext.FhirResponse.Encounters;
+        private List<Condition> Conditions => _httpContext.FhirResponse.Conditions;
+        private List<List> Lists => _httpContext.FhirResponse.Lists;
+
+        public StructuredConsultationsSteps(HttpSteps httpSteps, HttpContext httpContext)
+            : base(httpSteps)
+        {
+            _httpContext = httpContext;
+        }
+
+        [Given(@"I add the Consultations parameter")]
+        public void GivenIAddTheConsultationsParameter()
+        {
+            ParameterComponent param = new ParameterComponent();
+            param.Name = FhirConst.GetStructuredRecordParams.kConsultations;
+            _httpContext.HttpRequestConfiguration.BodyParameters.Parameter.Add(param);
+        }
+
+        [Then(@"The Consultations Resources are Valid")]
+        public void GivenTheConsultationsResourcesareValid()
+        {
+            Lists.ForEach(list =>
+            {
+                Console.WriteLine(list.Title);
+
+            });
+
+
+        }
+
+
+    }
+}
---
+++ GPConnect.Provider.AcceptanceTests/StructuredConsultations.feature
@@ -0,0 +1,29 @@
+﻿@structuredrecord
+Feature: StructuredConsultations
+
+
+@1.3.1
+Scenario Outline: Verify Consultations structured record for a Patient with Immunizations
+	Given I configure the default "GpcGetStructuredRecord" request
+		And I add an NHS Number parameter for "<Patient>"
+		And I add the Consultations parameter
+	When I make the "GpcGetStructuredRecord" request
+	Then the response status code should indicate success
+		#And the response should be a Bundle resource of type "collection"
+		#And the response meta profile should be for "structured"
+		#And the patient resource in the bundle should contain meta data profile and version id
+		#And if the response bundle contains a practitioner resource it should contain meta data profile and version id
+		#And if the response bundle contains an organization resource it should contain meta data profile and version id
+		#And the Bundle should be valid for patient "<Patient>"
+		#And the Patient Id should be valid
+		#And the Practitioner Id should be valid
+		#And the Organization Id should be valid 
+		#And The Immunization Resources are Valid
+		And The Consultations Resources are Valid
+		#And The Immunization Resources Do Not Include Not In Use Fields
+		#And the Bundle should contain "1" lists
+		#And The Immunization List is Valid
+		#And The Structured List Does Not Include Not In Use Fields
+	Examples:
+	| Patient  |
+	| patient2 |
